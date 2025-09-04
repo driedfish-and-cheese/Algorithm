0579eee4eafe767ebcdef8119c8415838a6313e6
driedfish-and-cheese <138121622+driedfish-and-cheese@users.noreply.github.com>
2025-09-04 19:35:07 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            int[] arr1 = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(arr1);
+            answer[i] = arr1[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
