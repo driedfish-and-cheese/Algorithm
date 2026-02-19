04f128e391a12bdb3660ded5e0bd9ae976d7b726
driedfish-and-cheese <138121622+driedfish-and-cheese@users.noreply.github.com>
2026-02-20 00:08:11 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 632 ms, Memory: 67220 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+visited = [False]*(n+1)
+cnt = 0
+def bfs(n):
+    q = deque([n])
+    visited[1] = True
+    while q:
+        v = q.popleft()
+        for nv in graph[v]:          
+            if not visited[nv]:
+                visited[nv] = True
+                q.append(nv)
+                
+for i in range(1,n+1):
+    if not visited[i]:
+        bfs(i)
+        cnt +=1
+print(cnt)
+    
+            
+    
+    
